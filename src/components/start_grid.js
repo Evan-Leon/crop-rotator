4b591412c263e4b991db341ab85d9b7bb2b7fc13
Evan-Leon <evan.leon.j@gmail.com>
2022-04-18 11:57:53 -0400

    Add state hooks to startgrid component

--- src/components/start_grid.js
+++ src/components/start_grid.js
@@ -1,12 +1,16 @@
-
+import React, {useState} from 'react';
 export default function StartGrid (){
-
+    const [numGrids, setNumGrids] = useState(0);
     return (
         <div>
             <h1>Choose your Grid</h1>
-            <label> Choose Number of Plots:
-                <input type="number"></input>
-            </label>
+            <form action="">
+                <label> Choose Number of Plots:
+                    <input type="number"  onChange={e => setNumGrids(numGrids + e.target.value)}></input>
+                </label>
+                <br/>
+                <button> Start Planting </button>
+            </form>
         </div>
     )
 }
