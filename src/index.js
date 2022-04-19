24a4f6b4946576a8d56f874dfcd2ece831bf82ce
Evan-Leon <evan.leon.j@gmail.com>
2022-04-19 13:49:11 -0400

    Initial steps to set up redux store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-
+import { createStore, applyMiddleware } from 'redux'; //change to configureStore
+import { Provider } from 'react-redux';
+const store = createStore(rootReducer) //configureStore (new way uses create store but has some good preconfigured options)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 
