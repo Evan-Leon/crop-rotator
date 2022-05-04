7ca82893c59a8348d6f311573d06f4c7e19a1988
Evan-Leon <evan.leon.j@gmail.com>
2022-05-04 11:12:36 -0400

    Change from redux, get grids to display after input

--- src/components/start_grid.js
+++ src/components/start_grid.js
@@ -1,6 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
+import './start_grid.css'
+// const initialGridSize = {num: 0};
+
+// function gridReducer(state, action) {
+//     switch (action.type) {
+//         case 'increase' :
+//             return {num: state.num + action.payload}
+//         default :
+//             throw new Error();
+//     }
+// }
 export default function StartGrid (){
     const [numGrids, setNumGrids] = useState(0);
+    if (numGrids === 0){
     return (
         <div>
             <h1>Choose your Grid</h1>
@@ -12,5 +24,26 @@ export default function StartGrid (){
                 <button> Start Planting </button>
             </form>
         </div>
-    )
+    )} else {
+        const gridArray = (num) => {
+            const finArr = [];
+            for (let i =1; i <= num; i +=1){
+                finArr.push(i);
+            }
+            return finArr;
+        }
+        return (
+        <div>
+            <div>
+                    <h1>Number of Plots: {Number(numGrids)}</h1>
+            </div>
+            <div>
+                {console.log(Number(numGrids))}
+                {gridArray(numGrids).map(function(num){
+                    return <div className='grids' key={num}>{num}</div>
+                })}
+            </div>
+        </div>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux'; //change to configureStore
+// import { createStore, applyMiddleware } from 'redux'; //change to configureStore
 import { Provider } from 'react-redux';
-const store = createStore(rootReducer) //configureStore (new way uses create store but has some good preconfigured options)
+// const store = createStore(rootReducer) //configureStore (new way uses create store but has some good preconfigured options)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
+ 
 );
 
 
